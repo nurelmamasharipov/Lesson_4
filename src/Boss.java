6fadbf889c83da4d67ca80569936ca0cd40cb5d9
nurelmamasharipov <nurikmathematic@gmail.com>
2024-07-17 23:20:11 +0600

    Initial commit

---
+++ src/Boss.java
@@ -0,0 +1,24 @@
+public class Boss {
+    private int health;
+    private int damage;
+    private String defence;
+
+    public int getHealth() {
+        return health;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public String getDefence() {
+        return defence;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+    public void setDefence(String defence){
+        this.defence = defence;
+    }
+}
